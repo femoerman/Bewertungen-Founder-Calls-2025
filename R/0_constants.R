f5fd85767137b31d3fa85f09c58a7a1c2778b23f
Felix Moerman <felix.moerman@hotmail.com>
2025-06-18 15:58:28 +0000

    Removed secrets

--- R/0_constants.R
+++ R/0_constants.R
@@ -1,7 +1,6 @@
 #Variable for the deployment environment. Should be set to rstudio (ran from desktop) or posit (deployed online)
 env.deploy <- "Renku"
-redcap_url <- "https://redcap.zhaw.ch/api/"
-redcap_token = "4797CC2DED381C6CDBA9E560483A7622"
+source("/secrets/api-token.R")
 
-amounts <- read.csv("data/dizh_amounts.csv", sep = ";", header = T)
+amounts <- read.csv("/secrets/dizh_amounts.csv", sep = ";", header = T)
 colnames(amounts) <- c("ID", "kandidat_in", "Beantragte DIZH Gelder (CHF)", "Projekttitel")
